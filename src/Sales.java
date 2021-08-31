be292678d2c475799de8e559e1f74eaf1dc3fc3f
samhi113 <samhiggins113@gmail.com>
2021-08-31 12:38:21 -0400

    Finished Sales

--- src/Sales.java
+++ src/Sales.java
@@ -1,5 +1,3 @@
-import org.w3c.dom.html.HTMLOptGroupElement;
-
 import javax.swing.JOptionPane;
 
 public class Sales {
@@ -22,12 +20,21 @@ public class Sales {
      */
 
     public static void main(String[] args) {
-        int discount, numBuying;
-        double price, salesTax, subtotal, taxTotal, total;
+        int numBuying;
+        double price, discount, salesTax, subtotal, taxTotal, total;
 
-        price = Double.parseDouble(JOptionPane.showInputDialog("Whats the price for the item?"));
+        price = Double.parseDouble(JOptionPane.showInputDialog("Whats the price for the item? (XX.XX)"));
         discount = Integer.parseInt(JOptionPane.showInputDialog("What Discount percentage? (No % sign)"));
         numBuying = Integer.parseInt(JOptionPane.showInputDialog("How many are you buying?"));
         salesTax = Double.parseDouble(JOptionPane.showInputDialog("What's the tax %? (no % sign)"));
+
+        subtotal = price * numBuying;
+        taxTotal = subtotal * (salesTax/100);
+        total = (subtotal + taxTotal) * (1-(discount/100.0));
+        discount = subtotal * (discount / 100);
+
+        JOptionPane.showMessageDialog(null, "Subtotal: $" + subtotal + "\n" + "Tax: $" + taxTotal + "\n" + "Discount: $" + discount + "\n" + "Total: $" + total);
+
+        System.exit(0);
     }
 }
